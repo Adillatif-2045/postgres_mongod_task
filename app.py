c20d283f5d79058dfc7bf0549705176d6cf95285
Adillatif2045 <125536686+Adillatif2045@users.noreply.github.com>
2023-03-16 23:50:57 +0500

    Add files via upload
    both html task should be in templates directory 
    test.zip will be selected by user 
    also make to directories of static\uploads so extracted files will be stored in upload folder 
    make sure you have installed PostgreSQL and mongo environment in your system and should be connected with db

---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+import zipfile
+
+import psycopg2
+from flask import Flask, request, redirect, url_for, flash, render_template
+from werkzeug.utils import secure_filename
+import psycopg2
+import psycopg2.extras
+
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
+ALLOWED_EXTENSIONS = set(['zip'])
+
+
+app = Flask(__name__)
+
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "postgres"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
+            zip_ref.extractall(UPLOAD_FOLDER)
+            zip_ref.close()
+
+            # fetch one image from the extracted images
+            images_folder = os.path.join(UPLOAD_FOLDER, 'test')  # the folder where the extracted images are stored
+            images = os.listdir(images_folder)  # list all the files in the folder
+            if len(images) > 0:  # if there is at least one image file
+                image_path = os.path.join(images_folder, images[0])  # get the path of the first image file
+                cursor.execute("INSERT INTO upload (title) VALUES (%s)", (image_path,))
+                conn.commit()
+                return render_template('image.html', image_path=image_path)  # display the first image file
+
+            else:  # if there is no image file in the folder
+                flash('No image file found in the uploaded zip file')
+                return redirect(request.url)
+        else:  # if the uploaded file is not a zip file
+            flash('Allowed file type is zip')
+            return redirect(request.url)
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
